8126ff301500a898909412c6a9853390f562eb9f
adisarode <adityasarode610@gmail.com>
2024-02-18 03:29:26 +0530

    first commit

---
+++ Array_And_Pointers/program45.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main() {
+
+	int x = 10;
+	int y = 20;
+
+	std::cout<<"x="<<x<<" "<<"y="<<y<<std::endl;
+
+	x=y;
+
+	std::cout<<"x= "<<x<<" "<<"y= "<<y<<std::endl;
+
+	return 0;
+}
