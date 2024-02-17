25b0d90c22f5b49b0a11073b362351e3405382c7
adisarode <adityasarode610@gmail.com>
2024-02-18 03:35:23 +0530

    first commit

---
+++ Array_And_Pointers/program48.cpp
@@ -0,0 +1,24 @@
+//Operation on Pointer
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int arr[] = {10,20,30,40,50};
+
+	int *iptr = &(arr[0]);
+
+	cout<<*iptr<<endl;
+
+	iptr++;
+
+	*iptr = 70;
+
+	for(int i=0;i<5;i++) {
+	
+		cout<<arr[i]<<endl;
+	}
+
+	return 0;
+}
