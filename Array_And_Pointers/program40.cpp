543f9ff677e2637720c09f87841b48bbdb8299d2
adisarode <adityasarode610@gmail.com>
2024-02-18 03:20:21 +0530

    Pointer Subtraction with integer

--- Array_And_Pointers/program40.cpp
+++ Array_And_Pointers/program40.cpp
@@ -1,14 +1,16 @@
 //pointer subtraction with integer
 #include<iostream>
 
+using namespace std;
+
 int main() {
 
 	int arr[] = {10,20,30,40,50};
 
 	int *ptr = &arr[1];
 
-	std::cout<<ptr<<std::endl;
-	std::cout<<*(ptr-1)<<std::endl;//10
+	cout<<ptr<<endl;
+	cout<<*(ptr-1)<<endl;//10
 
 	return 0;
 }
