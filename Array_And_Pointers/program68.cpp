5df293588044c6897d32ab34a991f10300882246
adisarode <adityasarode610@gmail.com>
2024-02-18 14:24:01 +0530

    Pointer to an Pointer

---
+++ Array_And_Pointers/program68.cpp
@@ -0,0 +1,25 @@
+//Pointer to an Array
+#include<iostream>
+
+using namespace std;
+int main() {
+
+	char carr1[3] = {'A','B','c'};
+	char carr2[3] = {'D','E','F'};
+
+	char *ptr1 = &carr1[1];
+	char *ptr2 = carr1;
+	char (*ptr3)[3] = &carr1;
+
+	cout<<**ptr3<<endl;//A
+/*	ptr3++;
+	cout<<**ptr3<<endl;//D
+
+	cout<<*ptr1<<endl;//B
+	ptr1++;
+	cout<<*ptr1<<endl;//C
+*/
+	cout<<*(*ptr3+2)<<endl;//c
+
+	return 0;
+}
